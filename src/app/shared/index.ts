210eaafeeba7bcff83da61d0fdf6f6c14e3e2dca
Duli Chand Prajapati <inbox.duli@gmail.com>
2019-02-11 23:04:13 +0530

    http error handler

--- src/app/shared/index.ts
+++ src/app/shared/index.ts
@@ -1,5 +1,6 @@
 
-export { BaseService } from './services/base.service';
+export { BaseService} from './services/base.service';
+export {ErrorHandlerService} from './httpErrorHandler/error-handler.service'
 export { OrdersummaryElement, UserLogin, Interface} from './interfaces';
 export { AdminGuard } from './guards/admin.guard';
 export { MatModule } from './mat.module';
