19bc6ce0211bfacace79633b57147ef982765902
Duli Chand Prajapati <inbox.duli@gmail.com>
2019-02-13 16:17:06 +0530

    review changes

--- src/app/common/header/header.component.ts
+++ src/app/common/header/header.component.ts
@@ -1,113 +1,43 @@
-// import { Component, OnInit } from '@angular/core';
-// import { routerNgProbeToken } from '@angular/router/src/router_module';
-// import { Router } from '@angular/router';
-
-// @Component({
-//   selector: 'app-header',
-//   templateUrl: './header.component.html',
-//   styleUrls: ['./header.component.css']
-// })
-// export class HeaderComponent implements OnInit {
-
-//   constructor(private router: Router) { }
-
-//   ngOnInit() {
-//   }
-
-//   logout(){
-//     this.router.navigate(['/core/login']);
-//   }
-// }
-
 import { Component, OnInit } from '@angular/core';
-
-import { Router,ActivatedRoute} from '@angular/router';
-
-import { LocalStorageService } from 'src/app/shared/services/localStorageService';
-
-
-
+import { Router, ActivatedRoute } from '@angular/router';
+import {LocalStorageService} from '../../shared/services/localStorageService';
 
 @Component({
-
-selector: 'app-header',
-
-templateUrl: './header.component.html',
-
-styleUrls: ['./header.component.css']
-
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.css']
 })
 
-export class HeaderComponent implements OnInit{
+export class HeaderComponent implements OnInit {
+    public hiddenHeader: boolean = true;
+    public headerValue: any;
+    constructor(private router: Router,
+        private localStorage: LocalStorageService,
+        private activeRoute: ActivatedRoute
+    ) { }
 
-public hiddenHeader:boolean=true;
+    ngOnInit() {
+    }
 
-public headerValue:any;
+    ngAfterContentChecked() {
+        this.headerValue = this.localStorage.getKey('admin');
+        this.setHeader();
+    }
 
+    setHeader() {
+        if (this.headerValue != null) {
+            this.hiddenHeader = false;
+        }
+        else {
+            this.hiddenHeader = true;
+        }
+    }
 
-
-
-constructor(private router: Router,
-
-private localStorage: LocalStorageService,
-
-private activeRoute:ActivatedRoute
-
-) { }
-
-
-
-
-ngOnInit() {
-
-}
-
-
-
-
-ngAfterContentChecked() {
-
-this.headerValue = this.localStorage.getKey('admin');
-
-this.setHeader();
-
-
-
-
-}
-
-setHeader() {
-
-if (this.headerValue !=null) {
-
-this.hiddenHeader=false;
-
-}
-
-else {
-
-this.hiddenHeader=true;
-
-} 
-
-
-
-
-}
-
-
-
-
-logout(){
-
-localStorage.clear();
-
-this.router.navigate(['login'], { relativeTo: this.activeRoute });
-
-this.hiddenHeader=true;
-
-this.headerValue=null;
-
-}
+    logout() {
+        localStorage.clear();
+        this.router.navigate(['login'], { relativeTo: this.activeRoute });
+        this.hiddenHeader = true;
+        this.headerValue = null;
+    }
 
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-
 import { Router, ActivatedRoute } from '@angular/router';
 import { LocalStorageService } from '../shared/services/localStorageService';
 import { AuthService } from '../shared/services/authService';
+import { ToasterService } from '../core/services';
 
 @Component({
   selector: 'app-login',
@@ -11,7 +11,12 @@ import { AuthService } from '../shared/services/authService';
 })
 export class LoginComponent implements OnInit {
 
-  constructor(private router: Router,private localStorage:LocalStorageService,private authService:AuthService,private activateRoute:ActivatedRoute) { }
+  constructor(
+    private router: Router,
+    private localStorage:LocalStorageService,
+    private authService:AuthService,
+    private activateRoute:ActivatedRoute,
+    private tosterService:ToasterService) { }
 
 
 username: string;
@@ -25,7 +30,7 @@ password: string;
       this.authService.getAuthenticate(getToken);
      this.router.navigate(['/dashboard'],{relativeTo:this.activateRoute});
     }else {
-      alert("Invalid credentials");
+       this.tosterService.showToaster('Invalid credentials');
     }
   }
   }
